7571d2662f3a8968efcc186b2be84e345e777ebd
Mateus Vilaverde <mateusvilaverde@gmail.com>
2019-01-11 19:10:50 -0200

    First commit, but almost done

--- js/app.js
+++ js/app.js
@@ -20,6 +20,9 @@ var allFeeds = [
     }, {
         name: 'Linear Digressions',
         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
+    }, {
+        name: 'My Tests',
+        url: 'anything'
     }
 ];
 
